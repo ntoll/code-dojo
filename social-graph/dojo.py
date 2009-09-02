69871a71aec07ca33e4b8fe3878da610063fcdca
Nicholas Tollervey <ntoll@ntoll.org>
2009-09-02 22:19:32 +0100

    First draft of social-network. Comments most welcome but please make sure you read the appropriate README files first. ;-)

---
+++ social-graph/dojo.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+"""
+An exercise in social graphing with Twitter and Graphviz
+
+The Twitter API is documented here:
+
+http://apiwiki.twitter.com/Twitter-API-Documentation
+
+Use the imported Twitter class thus:
+
+    twitter = Twitter('username', 'password')
+    # notice how the method calls mirror the RESTful HTTP calls of the Twitter
+    # API
+    twitter.statuses.public_timeline()
+    twitter.friends.ids.ntoll()
+    twitter.followers.ids.ntoll()
+    twitter.users.show(user_id=819606)
+    # the results are returned as dictionaries mirroring the json content
+
+Use the CreateGraph(edge_list, filename) function to create a png
+image with Graphviz.
+"""
+import subprocess
+from os import execvp
+from string import Template
+from twitter.api import Twitter
+
+###################
+# Utility Functions
+###################
+
+def create_dot_file(edge_list, root_node=None):
+    """
+    Generates a representation of the network in Graphviz's dot language
+
+    edge_list: a list of the connections (edges in the directed graph) within
+    the network. e.g. [[12345, 54321], [12345, 98765]] (using Twitter user ids)
+
+    root_node: the output file without the png extension (optional)
+    """
+    # Generate the dot language input - could have used a template language like
+    # Cheetah but decided this could be an exercise for the user... using
+    # Python's built-in template string handling
+    edges = ' '.join(['%d -> %d;'%(src, tgt) for src, tgt in edge_list])
+    if root_node:
+        # Visually identify the important "root" node
+        node_def = 'node [shape = doublecircle]; %d; node [shape = circle];'%root_node
+        graph = 'digraph G { %s %s }'%(node_def, edges)
+    else:
+        graph = 'digraph G { %s }'%edges
+    return graph
+
+def create_styled_dot_file(user_list, edge_list):
+    """
+    Generates a representation of the network in Graphviz's dot language with
+    additional stylistic enhancements to make it look pretty. 
+
+    node_list: a list of TwitterUser objects that are the nodes in the graph
+
+    edge_list: a list of the connections (edges in the directed graph) within
+    the network. e.g. [[12345, 54321], [12345, 98765]] (using Twitter user ids)
+    """
+     
+    NODE = '$id [label=< <table border="0" cellborder="0" cellspacing="0"'\
+            ' bgcolor="#CCCCCC"> <tr> <td colspan="2" cellpadding="2"'\
+            ' align="center" bgcolor="#33CCFF"> <font face="Helvetica Bold">'\
+            '$id</font> </td> </tr> $rows </table> >]'
+    ATTRIBUTE = '<tr> <td align="left" cellpadding="2"><font face="Helvetica'\
+            ' Bold">$key</font></td> <td align="left" cellpadding="2">$value'\
+            '</td> </tr>'
+    node = Template(NODE)
+    attribute = Template(ATTRIBUTE)
+    nodes = '\n'.join([node.substitute(id=u,
+        rows='\n'.join([attribute.substitute(key=k, value=v) for k, v in
+        d.iteritems()])) for u, d in user_list.iteritems()])
+    edges = ' '.join(['%s -> %s;'%(src, tgt) for src, tgt in edge_list])
+    graph = 'digraph G { node [ fontname = "Helvetica" fontsize = 8 shape ='\
+            ' "plaintext" ] %s %s }'%(nodes, edges)
+    return graph
+
+def create_graph(dot, filename="network"):
+    proc = subprocess.Popen('dot -Tpng > %s.png' % filename,
+                            shell=True,
+                            stdin=subprocess.PIPE
+                        )
+    proc.communicate(dot.encode('utf_8'))
+    #execvp('open', ['open', '%s.png'%filename,])
---
+++ social-graph/test_dojo.py
@@ -0,0 +1,55 @@
+"""
+Nosetests for dojo.py
+
+see http://somethingaboutorange.com/mrl/projects/nose/
+"""
+import dojo
+
+def create_dot_file_test():
+    # A very contrived directed graph as a list...
+    edges = [
+            [1, 2], 
+            [1, 3],
+            [1, 4],
+            [1, 5],
+            [1, 6],
+            [2, 1],
+            [2, 7],
+            [3, 2],
+            [3, 4],
+            [3, 7],
+            [4, 1],
+            [4, 7],
+            [5, 6],
+            [5, 7],
+            [6, 1],
+            [7, 6],
+            ]
+    # Test without root node
+    expected = 'digraph G { 1 -> 2; 1 -> 3; 1 -> 4; 1 -> 5; 1 -> 6; 2 -> 1; 2'\
+            ' -> 7; 3 -> 2; 3 -> 4; 3 -> 7; 4 -> 1; 4 -> 7; 5 -> 6; 5 -> 7; 6'\
+            ' -> 1; 7 -> 6; }'
+    assert expected == dojo.create_dot_file(edges)
+
+    # Test with root node
+    expected = 'digraph G { node [shape = doublecircle]; 3; node [shape ='\
+            ' circle]; 1 -> 2; 1 -> 3; 1 -> 4; 1 -> 5; 1 -> 6; 2 -> 1; 2 -> 7;'\
+            ' 3 -> 2; 3 -> 4; 3 -> 7; 4 -> 1; 4 -> 7; 5 -> 6; 5 -> 7; 6 -> 1;'\
+            ' 7 -> 6; }'
+    assert expected == dojo.create_dot_file(edges, root_node=3)
+
+def create_styled_dot_file_test():
+    # Some more contrived examples
+    users = {
+        'ntoll': {'following': 53, 'followers': 172, 'full name': 'Nicholas Tollervey'}, 
+        'bob': {'full name': 'Bob T.Builder'}, 
+        'fred': {'full name': 'Fred Blogs'}
+        }
+    edges = [
+            ['ntoll', 'fred'], 
+            ['fred', 'ntoll'], 
+            ['ntoll', 'bob'], 
+            ['fred', 'bob'],
+            ]
+    expected = 'digraph G { node [ fontname = "Helvetica" fontsize = 8 shape = "plaintext" ] ntoll [label=< <table border="0" cellborder="0" cellspacing="0" bgcolor="#CCCCCC"> <tr> <td colspan="2" cellpadding="2" align="center" bgcolor="#33CCFF"> <font face="Helvetica Bold">ntoll</font> </td> </tr> <tr> <td align="left" cellpadding="2"><font face="Helvetica Bold">following</font></td> <td align="left" cellpadding="2">53</td> </tr>\n<tr> <td align="left" cellpadding="2"><font face="Helvetica Bold">followers</font></td> <td align="left" cellpadding="2">172</td> </tr>\n<tr> <td align="left" cellpadding="2"><font face="Helvetica Bold">full name</font></td> <td align="left" cellpadding="2">Nicholas Tollervey</td> </tr> </table> >]\nbob [label=< <table border="0" cellborder="0" cellspacing="0" bgcolor="#CCCCCC"> <tr> <td colspan="2" cellpadding="2" align="center" bgcolor="#33CCFF"> <font face="Helvetica Bold">bob</font> </td> </tr> <tr> <td align="left" cellpadding="2"><font face="Helvetica Bold">full name</font></td> <td align="left" cellpadding="2">Bob T.Builder</td> </tr> </table> >]\nfred [label=< <table border="0" cellborder="0" cellspacing="0" bgcolor="#CCCCCC"> <tr> <td colspan="2" cellpadding="2" align="center" bgcolor="#33CCFF"> <font face="Helvetica Bold">fred</font> </td> </tr> <tr> <td align="left" cellpadding="2"><font face="Helvetica Bold">full name</font></td> <td align="left" cellpadding="2">Fred Blogs</td> </tr> </table> >] ntoll -> fred; fred -> ntoll; ntoll -> bob; fred -> bob; }'
+    assert expected == dojo.create_styled_dot_file(users, edges)
